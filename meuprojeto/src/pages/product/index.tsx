1b2cd056d23a335304eb72b2417a2b2adb8874f2
Matheus Miyagui <matheus_miyagui@hotmail.com>
2023-02-13 09:50:34 -0300

    criando links

--- meuprojeto/src/pages/product/index.tsx
+++ meuprojeto/src/pages/product/index.tsx
@@ -1,5 +1,12 @@
+import Link from 'next/link'
+
 export default function Product() {
     return (
-        <h1>Página de Produtos</h1>
+        <>
+          <h1>Página de Produtos</h1>
+          <Link legacyBehavior href="/">
+             <a>Voltar</a>
+          </Link>
+        </>
     )
 }
