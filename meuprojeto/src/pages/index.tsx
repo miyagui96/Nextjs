2356a48177b6467035b486a0d871b19e227d9572
Matheus Miyagui <matheus_miyagui@hotmail.com>
2023-02-14 09:32:10 -0300

    background

--- meuprojeto/src/pages/index.tsx
+++ meuprojeto/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import styles from 'styles/Home.module.css'
 
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -10,7 +11,15 @@ export default function Home() {
         <meta name="keywords" content="Roupas, Calçados e Bonés"></meta>
         <meta name="description" content="Encontre a melhor roupa pra você"></meta>
       </Head>
-      <h1 className={styles.title}>Olá, bem vindo ao mundo Mágico de Oz.</h1>
+      <div>
+        <h1 className={styles.title}>Olá, bem vindo ao mundo Mágico de Oz.</h1>
+        <Image 
+          src="/images/city.jpg"
+          width="300"
+          height="400"
+          alt="Cidade da noite"
+        />
+      </div>
     </>
   )
 }
